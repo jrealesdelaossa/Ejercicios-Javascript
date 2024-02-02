c83b60400284b9481f12a3f2b8f27b1928ce27b6
jrealesdelaossa <103285208+jrealesdelaossa@users.noreply.github.com>
2024-02-02 17:31:13 +0000

    Initial commit

---
+++ apple.js
@@ -0,0 +1,7 @@
+const applePrice = Document.getElementById('apple-price');
+const appleNumber = Document.getElementById('apple-number');
+const btnCalculate = Document.getElementById('calculate-apple-price');
+
+btnCalculate.addEventListener('click', () => {
+   alert("tes") 
+});
