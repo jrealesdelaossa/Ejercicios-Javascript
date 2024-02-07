da13f534f167cfe0de4c2f553e990e56263a7e55
jrealesdelaossa <jrealesdelaossa@gmail.com>
2024-02-07 11:37:29 -0500

    problema uno terminado

--- apple.js
+++ apple.js
@@ -1,7 +1,22 @@
-const applePrice = Document.getElementById('apple-price');
-const appleNumber = Document.getElementById('apple-number');
-const btnCalculate = Document.getElementById('calculate-apple-price');
+const applePrice = document.querySelector("#apple-price");
+const appleNumber = document.getElementById("apple-number");
+const btnCalculate = document.getElementById("calculate-apple-price");
+const applePriceTotal = document.getElementById("apple-price-total");
+const totalApplePrice = document.getElementById("apple-price-result");
 
-btnCalculate.addEventListener('click', () => {
-   alert("tes") 
+btnCalculate.addEventListener("click", () => {
+  const price = parseInt(applePrice.value);
+  const number = parseFloat(appleNumber.value);
+  let totalPrice = price * number;
+  applePriceTotal.textContent = "$ " + totalPrice;
+
+  if (number > 2 && number <= 5) {
+    totalPrice = totalPrice * 0.9;
+  } else if (number > 5 && number <= 10) {
+    totalPrice = totalPrice * 0.85;
+  } else if (number > 10) {
+    totalPrice = totalPrice * 0.8;
+  }
+
+  totalApplePrice.textContent = "$ " + totalPrice;
 });
